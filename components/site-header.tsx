abc45213f31c749c84301f9d47f5640623175528
Devin Clark <devin.clark@socotra.com>
2023-05-21 19:15:36 -0500

    open the home page

---
+++ components/site-header.tsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+	return (
+		<header className='z-40 py-6 flex items-center flex-col mb-4 justify-center pb-5 gap-5'>
+			<h3 className='scroll-m-20 text-5xl font-extrabold tracking-tight'>
+				Chex
+			</h3>
+			<h5 className='text-md font-medium leading-none'>
+				😋 Split checks with friends 😋
+			</h5>
+		</header>
+	);
+};
+
+export default NavBar;
